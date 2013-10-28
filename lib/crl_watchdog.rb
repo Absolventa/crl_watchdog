fbd0bdcf900773cfbda31eb1585ff41e47ee9fe1
Carsten Zimmermann <carp@hacksocke.de>
2013-10-28 09:46:13 +0100

    CrlWatchdog supports CRL file input

--- lib/crl_watchdog.rb
+++ lib/crl_watchdog.rb
@@ -4,4 +4,16 @@ class CrlWatchdog
 
   VERSION = '0.0.1'
 
+  attr_reader :crl
+
+  def initialize crl_file
+    ensure_file_exists! crl_file
+    @crl = OpenSSL::X509::CRL.new File.read(crl_file)
+  end
+
+  private
+
+  def ensure_file_exists! file
+    raise ArgumentError.new("File not found: #{file}") unless File.exists?(file)
+  end
 end
---
+++ spec/crl_watchdog_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe CrlWatchdog do
+  let(:crl_file) { File.expand_path("../support/crl.pem", __FILE__) }
+
+  subject { described_class.new crl_file }
+
+  context 'with file input' do
+    it 'requires an existing file' do
+      expect do
+        described_class.new '/does/not/exist.pem'
+      end.to raise_error ArgumentError
+    end
+
+    it 'requires a valid certificate revokation list' do
+      expect do
+        described_class.new __FILE__
+      end.to raise_error OpenSSL::X509::CRLError
+    end
+
+    it 'does not complain about a valid crl file' do
+      expect do
+        described_class.new crl_file
+      end.not_to raise_error
+    end
+
+    it 'assigns OpenSSL::X509::CRL object to #crl' do
+      expect(subject.crl).to be_instance_of OpenSSL::X509::CRL
+    end
+  end
+
+end
